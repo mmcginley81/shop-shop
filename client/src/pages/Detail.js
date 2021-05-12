43195decaeb4131ca4bfd867c7d7d920093132fc
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-05-11 20:26:42 -0700

    working on cart functionality

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,10 +3,14 @@ import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 
 import { QUERY_PRODUCTS } from "../utils/queries";
-import spinner from '../assets/spinner.gif'
+
 
 import { useStoreContext } from "../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../utils/actions";
+
+import CART from "../components/Cart";
+
+import spinner from '../assets/spinner.gif'
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -64,6 +68,7 @@ function Detail() {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <CART />
     </>
   );
 };
