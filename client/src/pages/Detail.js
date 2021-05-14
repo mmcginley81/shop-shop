e9a46507ebbf5ed61a82e3c6e382d1a5bc63d75e
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-05-14 14:47:12 -0700

    added idbPromise functionality for cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -62,11 +62,15 @@ function Detail() {
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
+      
     } else {
     dispatch({
       type: ADD_TO_CART,
       product: { ...currentProduct, purchaseQuantity: 1 }
     });
+   
+    // if product isn't in the cart yet, add it to the current shopping cart
+    idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1});
    }
   };
 
@@ -75,6 +79,9 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+  
+    // upon removal from cart, delete the item from IndexedDB using the `currentProduct._id` to locate what to remove
+    idbPromise('cart', 'delete', { ...currentProduct });
   };
 
   return (
