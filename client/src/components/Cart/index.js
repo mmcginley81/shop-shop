b4439b9f1ef46c9c53e16e9680496819e1df84d6
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-05-14 17:05:37 -0700

    added stripe functionality need to create save order in the database

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,14 +1,20 @@
 import React, { useEffect } from "react";
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
+import { useLazyQuery } from '@apollo/react-hooks';
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
-import { idbPromise } from "../../utils/helpers"
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
+import { loadStripe } from '@stripe/stripe-js';
 import "./styles.css";
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+  const [getCheckout, { data}] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
       async function getCart() {
@@ -25,12 +31,35 @@ const Cart = () => {
     dispatch({ type: TOGGLE_CART });
   }
 
+  //for stripe
+  useEffect(() => {
+    if (data) {
+        stripePromise.then((res) => {
+            res.redirectToCheckout({ sessionId: data.checkout.session });
+        });
+    }
+  }, [data]);
+
   function calculateTotal() {
     let sum = 0;
     state.cart.forEach(item => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+
+  function submitCheckout(){
+      const productIds = [];
+
+      state.cart.forEach((item) => {
+          for (let i=0; i < item.purchaseQuantity; i++) {
+            productIds.push(item._id);
+          }
+      })
+
+      getCheckout({
+          variables: { products: productIds }
+      })
   }
 
   if (!state.cartOpen) {
@@ -58,7 +87,7 @@ const Cart = () => {
 
             {
               Auth.loggedIn() ?
-                <button>
+                <button onClick={submitCheckout}>
                   Checkout
               </button>
                 :
